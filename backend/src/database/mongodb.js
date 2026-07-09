6f6d30fe10bb5c37ff3fcb41edfbbdcef832ac99
tricksterr <jalenbell2017@gmail.com>
2026-07-09 19:33:12 -0400

    add mongodb

---
+++ backend/src/database/mongodb.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import { DB_URI, NODE_ENV } from "../config/env.js";
+
+if (!DB_URI) {
+  throw new Error("DB_URI is not defined in the environment variables");
+}
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(DB_URI, {
+      serverSelectionTimeoutMS: 5000,
+    });
+
+    console.log(`Connected to database in ${NODE_ENV} mode`);
+  } catch (error) {
+    console.error("Could not connect to database:", error);
+
+    process.exit(1);
+  }
+};
+
+export default connectDB;
