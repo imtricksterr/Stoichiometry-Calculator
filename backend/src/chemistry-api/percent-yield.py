accb47e8a5922d2386e9084ab4345d155e899290
tricksterr <jalenbell2017@gmail.com>
2026-07-28 19:43:49 -0400

    fix rounding

--- backend/src/chemistry-api/percent-yield.py
+++ backend/src/chemistry-api/percent-yield.py
@@ -6,4 +6,4 @@ app = FastAPI()
 
 @app.get("/percent-yield/{formula}")
 def calculate_perecent_yield(actual: float, theoretical: float):
-    return round((actual / theoretical) * 100)
+    return round((actual / theoretical) * 100, 2)
