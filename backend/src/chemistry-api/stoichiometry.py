9fd23f8477cbf1c41d7d6d4fa27ccaa031bb6313
tricksterr <jalenbell2017@gmail.com>
2026-08-02 22:20:08 -0400

    stoichiometry python logic

--- backend/src/chemistry-api/stoichiometry.py
+++ backend/src/chemistry-api/stoichiometry.py
@@ -1,10 +1,29 @@
 from fastapi import FastAPI
-from chempy.util import periodic
-
+from chempy import balance_stoichiometry
+from molar_mass import calculate_molar_mass, parse_formula
 
 app = FastAPI()
 
 @app.get("/stoichiometry/{formula}")
-def calculate_stoichiometry(given: str, moles: float, target: str):
+def calculate_stoichiometry(given: str, moles: float, target: str, reactants: list[str], products: list[str]):
 
-    return { "result": 18.015 } # placeholder
+    react, prod = balance_equation(reactants, products)
+
+    all_coeffs = {**react, **prod}
+
+    given_coeff = all_coeffs[given]
+    target_coeff = all_coeffs[target]
+
+    result = (moles / given_coeff) * target_coeff
+
+
+    return round(result, 3)
+
+def balance_equation(reactants: list, products: list):
+
+    reac = set(reactants)
+    prod = set(products)
+
+    return balance_stoichiometry(reac, prod)
+
+
