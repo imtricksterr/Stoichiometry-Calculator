9c481e39b21ba4511c2e0070bee20e90f13fdf35
tricksterr <jalenbell2017@gmail.com>
2026-08-03 17:42:59 -0400

    add limiting reagent python logic

--- backend/src/chemistry-api/limiting_reagent.py
+++ backend/src/chemistry-api/limiting_reagent.py
@@ -1,16 +1,23 @@
 from fastapi import FastAPI
-import periodictable
-
+from stoichiometry import balance_equation
+from molar_mass import calculate_molar_mass
 
 app = FastAPI()
 
 @app.get("/limiting-reagent/{formula}")
 def calculate_limiting_reagent(
-    r1_formula: str, r1_coeff: float, r1_mass: float,
-    r2_formula: str, r2_coeff: float, r2_mass: float,
-    product_formula: str, product_coeff: float
-):
+    reactants: list[str], products: list[str], masses: list[float]
+    ):
 
+    react, prod = balance_equation(reactants, products)
+    all_coeffs = {**react, **prod} 
+    reference_prod = list(prod.keys())[0]
+    reference_coeff = all_coeffs[reference_prod]
 
-    
-    return { "result": 18.015 } # placeholder
+    product_moles = {}
+
+    for formula, mass in zip(reactants, masses): 
+        moles = mass / calculate_molar_mass(formula)
+        product_moles[formula] = (moles / all_coeffs[formula]) * reference_coeff
+
+    return min(product_moles, key=product_moles.get)
