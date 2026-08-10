5a13906e50635b516abd161f08ad26e5a7916e2e
tricksterr <jalenbell2017@gmail.com>
2026-08-09 23:50:06 -0400

    cors app parameters

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,6 +14,8 @@ const app = express();
 app.use(
   cors({
     origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
 app.use(express.json());
