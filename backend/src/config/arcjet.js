82835bb5705d80a0be0768a5bd6144bb80ea93f8
tricksterr <jalenbell2017@gmail.com>
2026-08-30 19:07:43 -0400

    arcjet adjustments

--- backend/src/config/arcjet.js
+++ backend/src/config/arcjet.js
@@ -13,7 +13,7 @@ const aj = arcjet({
     tokenBucket({
       mode: "LIVE",
       refillRate: 5, // Refill 5 tokens per interval
-      interval: 10, // Refill every 10 seconds
+      interval: 20, // Refill every 20 seconds
       capacity: 10, // Bucket capacity of 10 tokens
     }),
   ],
